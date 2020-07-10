aaa3ddbbc2ab417330d462d7c31fc23be94b3ab1
Anirudh Babu <anirudhbabu2001@gmail.com>
2020-07-09 23:36:17 -0400

    Image Carousel Banner added

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,15 @@
+/* File Name    : app.js
+   Author's Name: Anirudh Babu
+   Website Name : index.html
+   File Description: A JavaScript file that injects text into index.html and gives some output
+    to the browser console.
+ */
 "use strict";
 // setup your IIFE (Immediately Invoked Function Expression)
 
 (function () {
     
-    //paragraph text injects
+    //paragraph text injects start here
     document.getElementById("IntroParagraph").innerText = 
     `Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn\'t impacted the experience of visiting. Plus, there may be some great deals. 
 
@@ -29,27 +35,114 @@
     document.getElementById("Gavdos").innerText = 
     `Near the more well-known Crete, Gavdos is the most southern island in Greece — and the most southern spot in Europe discounting the Canaries. Only accessible by ferry, the remote island has only about 50 permanent residents, and can feel like your private playground. Local legend has it that the island was the home of goddess Calypso, who kept Odysseus prisoner here. Today, a favorite tourist activity is to visit the spot believed to be her cave. Be warned, you won't find any luxury hotels here. While the romantically under-developed, super laid back island has a number of rooms for rent, those are limited, as the real draw here for many is the free, seaside camping.`;
     
+    //stores the user-typed values obtained from the form in suitably named variables
+    // and outputs them to the console in a pretty way (according to me)
     function StoreSubmitValues()
     {
+        let contactForm = document.getElementsByTagName("form")[0];
         let submitButton = document.getElementById("sendButton");
         submitButton.addEventListener("click", (event) =>
         {
+            //prevents reloading and reset
             event.preventDefault();
+
+            //gets the value entered by the user for each field and assigns them to variables
             let firstName = document.getElementById("firstName").value;
             let lastName = document.getElementById("lastName").value;
             let contactNumber = document.getElementById("contactNumber").value;
             let email = document.getElementById("email").value;
             let yourMessage = document.getElementById("yourMessage").value;
             
-            console.log(`%cFirst Name    : ${firstName}`, "background-color: #E0F8FA; color: #AD5204; font-size: 2em;");
-            console.log(`%cLast Name     : ${lastName}`, "background-color: #E0F8FA; color: #AD5204; font-size: 2em;");
-            console.log(`%cContact Number: ${contactNumber}`, "background-color: #E0F8FA; color: #AD5204; font-size: 2em;");
-            console.log(`%cEmail         : ${email}`, "background-color: #E0F8FA; color: #AD5204; font-size: 2em;");
-            console.log(`%cMessage       : ${yourMessage}`, "background-color: #E0F8FA; color: #AD5204; font-size: 2em;");
+            //enables automatic validation by HTML
+            contactForm.checkValidity = true;
+
+            //style applied for the console.log messages
+            let styler = "background-color: #E0F8FA; color: #AD5204; font-size: 2em; font-family: Algerian;";
+
+            //code to output to browser console
+            console.log(`%cFirst Name           : ${firstName}`, styler);
+            console.log(`%cLast Name            : ${lastName}`, styler);
+            console.log(`%cContact Number: ${contactNumber}`, styler);
+            console.log(`%cEmail                      : ${email}`, styler);
+            console.log(`%cMessage                : ${yourMessage}`, styler);
         });
         
     }
+    function LinkCarouselImages()
+    {
+        document.getElementsByTagName("article")[1].id = "FolegandrosArt";
+        let carouselImgFolegandros = document.getElementsByTagName("img")[0];
+        let linkTagFole = document.createElement("a");
+        linkTagFole.href = `#FolegandrosArt`;
+        linkTagFole.title = "Read more about Folegandros";
+        carouselImgFolegandros.parentNode.insertBefore(linkTagFole, carouselImgFolegandros);
+        linkTagFole.appendChild(carouselImgFolegandros);
+
+        document.getElementsByTagName("article")[2].id = "AlonissosArt";
+        let carouselImgAlonissos = document.getElementsByTagName("img")[1];
+        let linkTagAlon = document.createElement("a");
+        linkTagAlon.href = `#AlonissosArt`;
+        linkTagAlon.title = "Read more about Alonissos";
+        carouselImgAlonissos.parentNode.insertBefore(linkTagAlon, carouselImgAlonissos);
+        linkTagAlon.appendChild(carouselImgAlonissos);
+
+        document.getElementsByTagName("article")[3].id = "SpetsesArt";
+        let carouselImgSpetses = document.getElementsByTagName("img")[2];
+        let linkTagSpet = document.createElement("a");
+        linkTagSpet.href = `#SpetsesArt`;
+        linkTagSpet.title = "Read more about Spetses";
+        carouselImgSpetses.parentNode.insertBefore(linkTagSpet, carouselImgSpetses);
+        linkTagSpet.appendChild(carouselImgSpetses);
+
+        document.getElementsByTagName("article")[4].id = "AmorgosArt";
+        let carouselImgAmorgos = document.getElementsByTagName("img")[3];
+        let linkTagAmor = document.createElement("a");
+        linkTagAmor.href = `#AmorgosArt`;
+        linkTagAmor.title = "Read more about Amorgos";
+        carouselImgAmorgos.parentNode.insertBefore(linkTagAmor, carouselImgAmorgos);
+        linkTagAmor.appendChild(carouselImgAmorgos);
+
+        document.getElementsByTagName("article")[5].id = "SyrosArt";
+        let carouselImgSyros = document.getElementsByTagName("img")[4];
+        let linkTagSyro = document.createElement("a");
+        linkTagSyro.href = `#SyrosArt`;
+        linkTagSyro.title = "Read more about Syros";
+        carouselImgSyros.parentNode.insertBefore(linkTagSyro, carouselImgSyros);
+        linkTagSyro.appendChild(carouselImgSyros);
+
+        document.getElementsByTagName("article")[6].id = "MilosArt";
+        let carouselImgMilos = document.getElementsByTagName("img")[5];
+        let linkTagMilo = document.createElement("a");
+        linkTagMilo.href = `#MilosArt`;
+        linkTagMilo.title = "Read more about Milos";
+        carouselImgMilos.parentNode.insertBefore(linkTagMilo, carouselImgMilos);
+        linkTagMilo.appendChild(carouselImgMilos);
+
+        document.getElementsByTagName("article")[7].id = "HydraArt";
+        let carouselImgHydra = document.getElementsByTagName("img")[6];
+        let linkTagHydr = document.createElement("a");
+        linkTagHydr.href = `#HydraArt`;
+        linkTagHydr.title = "Read more about Hydra";
+        carouselImgHydra.parentNode.insertBefore(linkTagHydr, carouselImgHydra);
+        linkTagHydr.appendChild(carouselImgHydra);
+
+        document.getElementsByTagName("article")[8].id = "IthacaArt";
+        let carouselImgIthaca = document.getElementsByTagName("img")[7];
+        let linkTagItha = document.createElement("a");
+        linkTagItha.href = `#IthacaArt`;
+        linkTagItha.title = "Read more about Ithaca";
+        carouselImgIthaca.parentNode.insertBefore(linkTagItha, carouselImgIthaca);
+        linkTagItha.appendChild(carouselImgIthaca);
+
+        document.getElementsByTagName("article")[9].id = "GavdosArt";
+        let carouselImgGavdos = document.getElementsByTagName("img")[8];
+        let linkTagGavd = document.createElement("a");
+        linkTagGavd.href = `#GavdosArt`;
+        linkTagGavd.title = "Read more about Gavdos";
+        carouselImgGavdos.parentNode.insertBefore(linkTagGavd, carouselImgGavdos);
+        linkTagGavd.appendChild(carouselImgGavdos);
+    }
     window.addEventListener("load", StoreSubmitValues);
-    
+    window.addEventListener("load", LinkCarouselImages);
 })();
 
